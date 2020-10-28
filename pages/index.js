8c8136e81b10a8affda1005fbade56597e88cdd1
railek <jean@railek.com>
2020-10-28 16:53:57 +0200

    chore: setup next.js foundation

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Next.js</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+      </main>
+    </>
+  );
+}

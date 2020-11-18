9144931af47796bd5a159ce93d18b21861e2766d
railek <jean@railek.com>
2020-11-18 20:45:23 +0200

    feat: layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Head from 'next/head';
 import GlobalStyles from '../styles';
+import Layout from '../components/layout';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyles />
-      <Component {...pageProps} />
+      <Head>
+        <title>Next.js</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Next.js</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <h1>Welcome to Next.js!</h1>
-      </main>
+      <h1>Welcome to Next.js!</h1>
     </>
   );
 }
